2cb44f25797f9261a586c1b7afbd1df46dd60ad4
Wonder Alexander <grandpadavan@yandex.ru>
2024-03-22 21:54:20 +0300

    загружаю файлы проекта

--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,26 @@
 // @todo: Темплейт карточки
+const cardTemplate = document.querySelector('#card-template').content;
 
 // @todo: DOM узлы
+const cardsList = document.querySelector('.places__list');
 
 // @todo: Функция создания карточки
+function createCard(cardData, deleteFunction) {
+    const newCard = cardTemplate.querySelector('.card').cloneNode(true);
+    newCard.querySelector('.card__image').src = cardData.link;
+    newCard.querySelector('.card__image').alt = cardData.name;
+    newCard.querySelector('.card__title').textContent = cardData.name;
+    const deleteCardBtn = newCard.querySelector('.card__delete-button');
+    deleteCardBtn.addEventListener('click', function () {
+        deleteFunction(newCard);
+    });
+    return newCard;
+}
 
 // @todo: Функция удаления карточки
+const deleteCard = (element) => element.remove();
 
 // @todo: Вывести карточки на страницу
+initialCards.forEach( item => {
+    cardsList.append(createCard(item, deleteCard));
+})
